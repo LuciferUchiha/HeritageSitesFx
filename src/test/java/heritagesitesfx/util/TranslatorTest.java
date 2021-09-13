bc51b5e2868caedf13e2c8ae9c642f4446c3ac9a
Lucifer Uchiha <rowla101@gmail.com>
2021-09-13 21:59:12 +0200

    Removed test that only worked with liberica

--- src/test/java/heritagesitesfx/util/TranslatorTest.java
+++ src/test/java/heritagesitesfx/util/TranslatorTest.java
@@ -13,20 +13,6 @@ import java.util.concurrent.TimeUnit;
 
 public class TranslatorTest {
 
-    @BeforeAll
-    public static void initToolkit()
-            throws InterruptedException {
-        final CountDownLatch latch = new CountDownLatch(1);
-        SwingUtilities.invokeLater(() -> {
-            new JFXPanel(); // initializes JavaFX environment
-            latch.countDown();
-        });
-
-        // That's a pretty reasonable delay... Right?
-        if (!latch.await(5L, TimeUnit.SECONDS))
-            throw new ExceptionInInitializerError();
-    }
-
     @BeforeEach
     public void resetToDefaultLocale() {
         Translator.setLocale(Translator.getDefaultLocale());
@@ -49,15 +35,5 @@ public class TranslatorTest {
         Assertions.assertEquals(Locale.GERMAN, Translator.getDefaultLocale());
         Assertions.assertEquals(Locale.GERMAN, Translator.getLocale());
         Assertions.assertEquals("Welterbe", Translator.get("application.title"));
-    }
-
-    @Test
-    public void testButtonCreation() {
-        Assertions.assertEquals("Welterbe", Translator.buttonForKey("application.title").getText());
-    }
-
-    @Test
-    public void testLabelCreation() {
-        Assertions.assertEquals("Welterbe", Translator.labelForKey("application.title").getText());
     }
 }
